8f3e5e0c61e6f220efba44707a87c72cbc1246bb
Gabriel Brinoti <gabrielcaastrobri@gmail.com>
2026-05-15 20:13:46 -0300

    Corrigido horario do aceite e adcionado acesso ao contrato completo

--- app.py
+++ app.py
@@ -773,7 +773,7 @@ def aceitar_contrato():
         SET aceitou_contrato = TRUE,
             data_aceite_contrato = %s
         WHERE id = %s
-    """, (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), aluno_id))
+    """, (datetime.utcnow() - timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S"), aluno_id)
 
     data_base = obter_data_base()
     data_agendamento = data_base.strftime("%Y-%m-%d")
